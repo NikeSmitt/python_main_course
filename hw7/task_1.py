9eb99d2b07d4517e3a68d83ce9d9c8761e33fce3
Nikita <grim322@bk.ru>
2020-11-15 21:53:23 +0300

    3 solved problems added

---
+++ hw7/task_1.py
@@ -0,0 +1,38 @@
+# 1. Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()), который должен
+# принимать данные (список списков) для формирования матрицы. Следующий шаг — реализовать перегрузку метода __str__()
+# для вывода матрицы в привычном виде. Далее реализовать перегрузку метода __add__() для реализации операции сложения
+# двух объектов класса Matrix (двух матриц). Результатом сложения должна быть новая матрица.
+#
+# Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы складываем
+# с первым элементом первой строки второй матрицы и т.д.
+
+class Matrix:
+
+    def __init__(self, matrix):
+        self.__matrix = matrix
+
+    def __str__(self):
+        matrix_rows = []
+        for row in self.__matrix:
+            matrix_rows.append(' '.join(f'{x:>2}' for x in row))
+        return '\n'.join(matrix_rows)
+
+    def __add__(self, other):
+        # проверяем, что матрицы имеют одинаковую размерность
+        if len(self.__matrix) == len(other.__matrix) and len(self.__matrix[0]) == len(other.__matrix[0]):
+            res_arrays = []
+            for i in range(len(self.__matrix)):
+                res_arrays.append([other.__matrix[i][j] + self.__matrix[i][j] for j in range(len(self.__matrix[0]))])
+            return Matrix(res_arrays)
+        else:
+            print('Value error: matrices have different quantity of elements')
+
+
+m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+m2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(m1)
+print(m1 + m2)
+
+m3 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+m4 = Matrix([[1, 2], [4, 5]])
+print(m3 + m4)
---
+++ hw7/task_2.py
@@ -0,0 +1,77 @@
+# Реализовать проект расчета суммарного расхода ткани на производство одежды. Основная сущность (класс) этого проекта
+# — одежда, которая может иметь определенное название. К типам одежды в этом проекте относятся пальто и костюм. У
+# этих типов одежды существуют параметры: размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и
+# H, соответственно. Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (V/6.5 +
+# 0.5), для костюма (2*H + 0.3). Проверить работу этих методов на реальных данных. Реализовать общий подсчет расхода
+# ткани. Проверить на практике полученные на этом уроке знания: реализовать абстрактные классы для основных классов
+# проекта, проверить на практике работу декоратора @property.
+
+
+class Clothes:
+
+    def __init__(self, fabric_cost=None):
+        self.fabric_cost = fabric_cost
+
+    def get_fabric_cost(self):
+        return self.fabric_cost
+
+    def __str__(self):
+        return f'Fabric cost: {self.get_fabric_cost():.2f}'
+
+    def __add__(self, other):
+        return Clothes(self.get_fabric_cost() + other.get_fabric_cost())
+
+
+class Coat(Clothes):
+
+    def __init__(self, size):
+        super().__init__()
+        if not isinstance(size, int):
+            raise ValueError(f'Invalid input size type: {type(size)}')
+        else:
+            self._size = size
+
+    def get_fabric_cost(self):
+        if self.fabric_cost is None:
+            self.fabric_cost = self._size / 6.5 + 0.5
+        return self.fabric_cost
+
+    def __str__(self):
+        return f'{super().__str__()} for coat'
+
+    @property
+    def size(self):
+        return self._size
+
+
+class Suit(Clothes):
+
+    def __init__(self, height):
+        super().__init__()
+        try:
+            self._height = float(height)
+        except Exception:
+            raise ValueError(f'Invalid input size type: {type(height)}')
+
+    def get_fabric_cost(self):
+        if self.fabric_cost is None:
+            self.fabric_cost = self._height * 2 + 0.3
+        return self.fabric_cost
+
+    def __str__(self):
+        return f'{super().__str__()} for suit'
+
+    @property
+    def height(self):
+        return self._height
+
+
+my_coat = Coat(42)
+my_suit = Suit(1.73)
+my_suit_for_work = Suit(1.73)
+
+print(my_coat)
+print(my_suit)
+
+all_clothes = my_suit + my_coat + my_suit_for_work
+print(all_clothes)
